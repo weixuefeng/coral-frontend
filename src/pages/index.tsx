dc7b816bbbca7afd3366dbbe810a363ca1cfb5fe
liukeke <liukekel@163.com>
2024-03-15 15:58:30 +0800

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
  * @Author:  
  * @Date: 2024-03-13 21:36:45
  * @LastEditors:  
- * @LastEditTime: 2024-03-15 14:45:11
+ * @LastEditTime: 2024-03-15 15:46:02
  * @FilePath: /coral-frontend/src/pages/index.tsx
  */
 
@@ -48,8 +48,12 @@ function Main() {
           <div className='nft'>
             <p className='tit'>PRODUCT NFT</p>
             <h3>Coral&nbsp;<span>CID NFT</span></h3>
-            <p className='mb-8'>Join Coral member pass with Coral CID NFT.<br /> Get special benefits and gain more profits!</p>
+            <p className='mb-6 md:mb-8'>Join Coral member pass with Coral CID NFT.<br /> Get special benefits and gain more profits!</p>
             <p>CID NFT is the most basic user unit, and each CID NFT has the ability to<br /> share the revenue of the Coral platform.</p>
+            <div className='nft-img block md:hidden mt-5 mb-3'>
+              <img className='img1' src="assets/image/cid_nft_bg.png" alt="nft-bg" />
+              <img className='img' src="assets/image/nft1.png" alt="nft" />
+            </div>
             <div className='nft-name'>
               <ul>
                 <li>
@@ -74,19 +78,20 @@ function Main() {
                   />
                 </li>
               </ul>
+              <div className='mint block md:hidden'>Mint</div>
             </div>
           </div>
-          <div className='nft-img'>
+          <div className='nft-img hidden md:block'>
             <img src="assets/image/cid_nft_bg.png" alt="nft-bg" />
             <img className='img' src="assets/image/nft1.png" alt="nft" />
           </div>
         </div>
       </div>
-      <div className='bg-redFA py-[82px] mt-[100px]'>
+      <div className='bg-redFA py-[25px] md:py-[82px] mt-[25px] md:mt-[100px]'>
         <div className='main officially'>
           <div className='text'>
             <p>CID NFTs are the primary assets in Coral's metaverse, functioning as 3D rendering engines powered by collaboration with DGX Cloud under NVIDIA.</p>
-            <p className='mt-10'>They support Coral‘s future metaverse scenes and generate tokens by providing rendering services, alongside AI capabilities, with potential for external computing power output.</p>
+            <p className='mt-5 md:mt-10'>They support Coral‘s future metaverse scenes and generate tokens by providing rendering services, alongside AI capabilities, with potential for external computing power output.</p>
           </div>
           {/* <ul>
             <li>
@@ -102,12 +107,12 @@ function Main() {
       </div>
       <div className='main'>
         <div className='cid-nft depin-nft'>
-          <div className='nft-img'>
+          <div className='nft-img hidden md:block'>
             <img src="assets/image/depin_nft_bg.png" alt="nft-bg" />
             <img className='img' src="assets/image/nft2.png" alt="nft" />
           </div>
           <div className='nft'>
-            <div className='best'>
+            <div className='best hidden md:block'>
               <img className='img1' src="assets/image/best.png" alt="" />
               <img className='img2' src="assets/image/best_val.png" alt="" />
             </div>
@@ -116,6 +121,11 @@ function Main() {
             <p>A superior NFT above CID.</p>
             <p>Enjoy the ability to manage and distribute the CID NFT.</p>
             <p>Each Depin NFT belongs to a verifiable data node, and the CID NFT managed by Depin NFT can obtain better asset returns.</p>
+            <div className='nft-img block md:hidden mt-5 mb-3'>
+              <img className='img1' src="assets/image/depin_nft_bg.png" alt="nft-bg" />
+              <img className='img' src="assets/image/nft2.png" alt="nft" />
+              <img className='img3' src="assets/image/best-h5.png" alt="nft" />
+            </div>
             <div className='nft-name'>
               <ul>
                 <li>
@@ -149,8 +159,8 @@ function Main() {
         <div className='main officially'>
           <div className='text'>
             <p>DePIN NFTs enhance CID NFTs with more computing power and ecosystem benefits, such as governance rights and increased token rewards. </p>
-            <p className='mt-10'>Acting as data processing nodes, each DePIN NFT receives allocated CID NFT computing power. Coral supplies iris recognition devices to DePIN NFT holders, enabling biometric verification and motion capture for the metaverse. DePIN NFTs conduct iris recognition, rewarding participants with Coral incentives. </p>
-            <p className='mt-10'>Additionally, DePIN NFTs can autonomously promote and sell CID NFTs, with proceeds primarily benefiting the equipment provider and supporting Coral's ecosystem and maintenance.</p>
+            <p className='mt-6 md:mt-10'>Acting as data processing nodes, each DePIN NFT receives allocated CID NFT computing power. Coral supplies iris recognition devices to DePIN NFT holders, enabling biometric verification and motion capture for the metaverse. DePIN NFTs conduct iris recognition, rewarding participants with Coral incentives. </p>
+            <p className='mt-6 md:mt-10'>Additionally, DePIN NFTs can autonomously promote and sell CID NFTs, with proceeds primarily benefiting the equipment provider and supporting Coral's ecosystem and maintenance.</p>
           </div>
           <div className='home-footer'>© 2024 Coral App.</div>
           {/* <ul>
@@ -190,7 +200,7 @@ const IncrementDecrementInput = ({ value, onInput, onIncrement, onDecrement }) =
         />
         <div className='add-minus' onClick={onDecrement}>-</div>
       </div>
-      <div className='mint'>Mint</div>
+      <div className='mint hidden md:block'>Mint</div>
     </div>
 
   );
