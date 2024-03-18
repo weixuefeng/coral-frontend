27517c4a483ff34674c438446b4f521d4f9c5844
liukeke <liukekel@163.com>
2024-03-18 11:21:33 +0800

    input

--- src/components/IncrementInput.tsx
+++ src/components/IncrementInput.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author:
  * @Date: 2024-03-15 18:03:48
- * @LastEditors:
- * @LastEditTime: 2024-03-15 19:27:39
+ * @LastEditors:  
+ * @LastEditTime: 2024-03-18 11:20:15
  * @FilePath: /coral-frontend/src/components/IncrementInput.tsx
  */
 import React, { Fragment, useState } from 'react'
@@ -17,12 +17,12 @@ export default function IncrementInput({ value, onInput, onIncrement, onDecremen
   return (
     <div>
       <div className="flex items-center justify-between">
-        <div className="add-minus" onClick={onIncrement}>
-          +
-        </div>
-        <input className="value" type="text" placeholder="1" required value={value} onChange={handleInputChange} />
         <div className="add-minus" onClick={onDecrement}>
           -
+        </div>
+        <input className="value" type="text" placeholder="1" required value={value} onChange={handleInputChange} />
+        <div className="add-minus" onClick={onIncrement}>
+          +
         </div>
       </div>
       <div className="mint hidden md:block" onClick={onMint}>
